228bc8b416dd8be067d551b272e252b6bb561177
franciscocalente <37421998+franciscocalente@users.noreply.github.com>
2019-05-08 00:04:28 -0300

    versão 1.4
    inclusao do arquivo conexao.php no dao

---
+++ dao/conexao.php
@@ -0,0 +1,17 @@
+
+<?php
+    class Conexao{
+        private $servidor_mysql ='localhost';
+        private $nome_banco ='livraria';
+        private $usuario ='root';
+        private $senha = '';
+        private $con;
+
+        public function getConexao(){ //pode tirar o getConexao só deixar Conexao construtor - ai na modelo.inc é so instanciar o objeto!
+            $this->con =  new PDO("mysql:host=$this->servidor_mysql;dbname=$this->nome_banco","$this->usuario","$this->senha");
+            return $this->con;
+        }
+
+    } 
+   
+?>   
