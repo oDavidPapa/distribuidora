cd52584008ffdb6a6da9f1dce050e9f1ac2864cc
Renan Muniz <fmrenan@outlook.com>
2019-05-10 01:46:17 -0300

    Versão 1.6
    Definido charset na conexão. Fim dos BUGS de codificação!!!

--- dao/Conexao.php
+++ dao/Conexao.php
@@ -8,7 +8,7 @@
         private $con;
 
         public function getConexao(){ //pode tirar o getConexao só deixar Conexao construtor - ai na modelo.inc é so instanciar o objeto!
-            $this->con =  new PDO("mysql:host=$this->servidor_mysql;dbname=$this->nome_banco","$this->usuario","$this->senha");
+            $this->con =  new PDO("mysql:host=$this->servidor_mysql;dbname=$this->nome_banco;charset=utf8","$this->usuario","$this->senha");
             return $this->con;
         }
 
