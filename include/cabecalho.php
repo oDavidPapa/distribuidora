d19c71bd3b3aeffc5352880cb744e04e1d2ea187
Renan Muniz <fmrenan@outlook.com>
2019-06-25 15:05:39 -0300

    modificado script
    Corrigido caminho do script

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -17,7 +17,7 @@
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="meuScript.js"></script>
+        <script src="include/meuScript.js"></script>
         <script src="node_modules/jquery/dist/jquery.js"></script>
         <script src="node_modules/popper.js/dist/umd/popper.js"></script>
         <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
