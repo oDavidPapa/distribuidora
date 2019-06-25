02b8f1a1d157bc4892ec71948d9e29b272c8a962
Renan Muniz <fmrenan@outlook.com>
2019-06-25 15:08:00 -0300

    Criada página
    Exibindo apenas as compras, precisa atualizar para exibir os itens de cada uma

---
+++ minhasCompras.php
@@ -0,0 +1,72 @@
+<?php
+    session_start();
+    $usuario = $_SESSION['usuario'];
+?>
+
+<?php 
+    include_once './include/cabecalho.php'; 
+    include_once './model/Compra.php';
+    include_once './dao/ClienteDAO.php';
+
+    $clienteDAO = new ClienteDAO();
+    $compras = $clienteDAO->getCompras();
+
+    function formatarData($pData){
+        return date('d/m/Y', $pData);
+    }
+?>
+
+<body>
+
+    <?php
+    if (isset($_SESSION['usuario'])) {
+        include_once './include/menuNavegacaoLogado.php';
+    } else {
+        include_once './include/menuNavegacao.php';
+    }
+   
+    ?>
+
+<body>
+
+    <div class="container py-5 mb-5 text-center">
+        <h4 class="text-center text-muted font-italic">Informações das compras</h4>
+        <table id="tabela" class="table table-striped table-bordered">
+
+
+            </div>
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Data</th>
+                    <th>Total</th>
+                    <th>Frete</th>
+                    <th></th>
+                    <!--<th></th>-->      
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                    foreach ($compras as $compra) {
+                        echo "<tr align='center'>";
+                        echo "<td>" . $compra->idCompra . "</td>";
+                        echo "<td>" .formatarData(strtotime($compra->dataCompra))."</td>";
+                        echo "<td>R$ ".number_format($compra->valorTotal, 2, ',', '.')."</td>";
+                        echo "<td>R$ ".number_format($compra->valorFrete, 2, ',', '.')."</td>";
+
+                        echo "<td>Visualizar</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+        
+</body>
+
+
+
+
+
+<?php include_once './include/rodape.php'; ?>
