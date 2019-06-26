835bdcffc2a7e70c4c4cfbbd509c1072eee46839
Renan Muniz <fmrenan@outlook.com>
2019-06-26 13:07:08 -0300

    v2.0
    Função de mais e menos do carrinho

--- include/meuScript.js
+++ include/meuScript.js
@@ -1,3 +1,15 @@
 function carregaCompras(){
    window.location.replace("controler/controlerMinhasCompras.php");
 }
+
+function mais(cont){
+   var quantidade = parseInt(document.getElementById("quantidade"+cont).value);
+   var idBebida= document.getElementById("idBebida"+cont).value;
+   window.location.assign("controler/carrinhoControler.php?opcao=7&quantidade="+quantidade+"&idBebida="+idBebida);
+}
+function menos(cont){
+   var quantidade = parseInt(document.getElementById("quantidade"+cont).value);
+   var idBebida= document.getElementById("idBebida"+cont).value;
+   window.location.assign("controler/carrinhoControler.php?opcao=8&quantidade="+quantidade+"&idBebida="+idBebida);
+}
+ 
